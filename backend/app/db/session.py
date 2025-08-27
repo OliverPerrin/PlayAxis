cb808ee8846bd4d7f3bff049beaaca702e42af42
Oliver Perrin <oliver.t.perrin@gmail.com>
2025-08-27 15:53:20 -0400

    Updated session.py to fix database issue

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -5,5 +5,13 @@ from app.core.config import settings
 
 from .base import Base
 
-engine = create_engine(settings.database_url, echo=True)
+engine = create_engine(settings.DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
