f41462cb5e0633ffc2db555be996824c2a26394d
Oliver Perrin <oliver.t.perrin@gmail.com>
2025-08-27 16:33:27 -0400

    Fixed an issue with security auth

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,12 +1,43 @@
 from datetime import datetime, timedelta, timezone
 from typing import Any, Union
-
-from jose import jwt
 from passlib.context import CryptContext
 
 from app.core.config import settings
 
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+
+from app.db.session import get_db
+from app.models.user import User
+from app.core.config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+    return user
 
 
 def create_access_token(subject: Union[str, Any], expires_delta: timedelta = None) -> str:
