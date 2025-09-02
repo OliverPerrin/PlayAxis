a77f04a6152be219342b915e214fbbca553d8a6d
Oliver Perrin <oliver.t.perrin@gmail.com>
2025-09-02 14:20:20 -0400

    Fixed some backend import issues

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-from .base import Base
+from .base_class import Base
 
 engine = create_engine(settings.DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
