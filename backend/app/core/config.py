cc96142dc2498c265a7f5ca9fc28d813124ebab2
Oliver Perrin <oliver.t.perrin@gmail.com>
2025-09-09 14:13:17 -0400

    Fixed Eventbrite API Access

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -14,12 +14,14 @@ class Settings(BaseSettings):
     TWITCH_CLIENT_SECRET: str | None = None
     EVENTBRITE_API_KEY: str | None = None
     EVENTBRITE_CLIENT_SECRET: str | None = None
+    EVENTBRITE_PRIVATE_TOKEN: str | None = None
+    EVENTBRITE_PUBLIC_TOKEN: str | None = None
 
     # API URLs
     EVENTBRITE_API_URL: str = "https://www.eventbriteapi.com/v3"
     WEATHER_API_URL: str = "https://api.open-meteo.com/v1"
-    TWITCH_API_URL: str = "https://localhost:8000/"
-    SPORTSBOOK_API_URL: str = "https://sportsbook-api.p.rapidapi.com/v1"
+    TWITCH_API_URL: str = "https://api.twitch.tv/helix/"
+    SPORTSBOOK_API_URL: str = "https://sportsradar-sportsbook-api.p.rapidapi.com"
 
     # Database
     DATABASE_URL: str
